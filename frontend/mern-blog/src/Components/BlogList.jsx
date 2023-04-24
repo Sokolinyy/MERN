b2a6d0dad0aeeaa27e3c30659ae83a5a7c77835c
Sokolinyy <kostyk.vasilii@gmail.com>
2023-04-24 16:55:32 +0200

    Fixing error

--- frontend/mern-blog/src/Components/BlogList.jsx
+++ frontend/mern-blog/src/Components/BlogList.jsx
@@ -9,7 +9,7 @@ const BlogList = () => {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/");
+        const response = await axios.get("http://localhost:5000/create-blog");
         if (response.data.message) {
           setMessage(response.data.message);
         } else {
